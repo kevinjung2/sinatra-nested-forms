2ffae967cee9fe0f749b04c788e5b7bc99cc37f6
Kevin Jung <kev.jung.ii@gmail.com>
2021-02-26 16:55:58 -0500

    commit tree 21fbd5010162515bb551ad6d0ed3f79869ae0d32

--- app/models/pirate.rb
+++ app/models/pirate.rb
@@ -1,2 +1,20 @@
 class Pirate
+  attr_accessor :name, :weight, :height
+
+  @@all = []
+
+  def initialize(name, weight, height)
+    @name = name
+    @weight = weight
+    @height = height
+    self.save
+  end
+
+  def self.all
+    @@all
+  end
+
+  def save
+    @@all << self
+  end
 end
